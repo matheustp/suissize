ca8f1d3d75211e32d10fb2084c4a10f8517745a5
Matheus Torres <torrespmatheus@gmail.com>
2017-06-24 23:58:47 -0300

    Fix line counter when using var

--- extension.js
+++ extension.js
@@ -1,110 +1,110 @@
 'use strict'
-const vscode = require('vscode')
-const standardFormat = require('./standard-format')
+const vscode = require( 'vscode' )
+const standardFormat = require( './standard-format' )
 
-const replaceParenthesisAndBrackets = (text) => {
-  const regexOpeningParenthesisAndBrackets = /(\{|\(|\[)(\b|\(|\'|\`|\"|\{|\[)/g
-  const regexClosingParenthesisAndBrackets = /(\b|\+|\-|\)|\'|\`|\"|\}|\])(\}|\]|\))/g
+const replaceParenthesisAndBrackets = ( text ) => {
+  const regexOpeningParenthesisAndBrackets = /(\{|\(|\[)(\b|\(|\'|\`|\"|\{|\[|\!|\/|\+|\-)/g
+  const regexClosingParenthesisAndBrackets = /(\b|\+|\-|\)|\'|\`|\"|\}|\]|\!|\/)(\}|\]|\))/g
   const fixNestedOpeningParenthesisAndBrackets = /(\{|\(|\[)(\{|\(|\[)/g
   const fixNestedClosingParenthesisAndBrackets = /(\}|\]|\))(\}|\]|\))/g
   const fixElse = /(\})(else)/g
-  return text.replace(fixNestedClosingParenthesisAndBrackets, `$1 $2`)
-    .replace(fixNestedOpeningParenthesisAndBrackets, `$1 $2`)
-    .replace(regexOpeningParenthesisAndBrackets, `$1 $2`)
-    .replace(regexClosingParenthesisAndBrackets, `$1 $2`)
-    .replace(fixElse, `$1 $2`)
+  return text.replace( fixNestedClosingParenthesisAndBrackets, `$1 $2` )
+    .replace( fixNestedOpeningParenthesisAndBrackets, `$1 $2` )
+    .replace( regexOpeningParenthesisAndBrackets, `$1 $2` )
+    .replace( regexClosingParenthesisAndBrackets, `$1 $2` )
+    .replace( fixElse, `$1 $2` )
 }
 
-const removeMultiLine = (text) => {
+const removeMultiLine = ( text ) => {
   const regexMultiLine = /(.)(\n\n)(\n)+/g
-  return text.replace(regexMultiLine, `$1$2`)
+  return text.replace( regexMultiLine, `$1$2` )
 }
 
-const fixSemiColon = (text) => {
-  return text.replace(/;(\w|\-|\+)/g, `; $1`)
+const fixSemiColon = ( text ) => {
+  return text.replace( /;(\w|\-|\+)/g, `; $1` )
 }
 
-const protectArrowFunction = (text) => {
-  return text.replace(/=>(( )*?\n(.|\s)*?\?(.|\s)*?:([^\n])*?(\n))/g, `=> {$1}`)
+const protectArrowFunction = ( text ) => {
+  return text.replace( /=>(( )*?\n(.|\s)*?\?(.|\s)*?:([^\n])*?(\n))/g, `=> {$1 }` )
 }
 
-const unproctectArrowFunction = (text) => {
-  return text.replace(/=>(\s)(\{)(([a-zA-Z0-9\(\)\=\%\&\!\.\+\-\/\*]|\s)*?\?([a-zA-Z0-9\(\)\=\%\&\!\.\+\-\/\*]|\s)*?:([a-zA-Z0-9\(\)\=\%\&\!\.\+\-\/\* ])*?\n)(\})/g, `=> $3`)
+const unproctectArrowFunction = ( text ) => {
+  return text.replace( /=>(\s)(\{)(([a-zA-Z0-9\(\)\=\%\&\!\.\+\-\/\*]|\s)*?\?([a-zA-Z0-9\(\)\=\%\&\!\.\+\-\/\*]|\s)*?:([a-zA-Z0-9\(\)\=\%\&\!\.\+\-\/\* ])*?\n)(\})/g, `=> $3` )
 }
 
-const formatErrorMessage = (text) => {
-  return text.replace(/\((\d+)/g, `(Line $1`).replace(/\:(\d+)/g, `: Col $1`)
+const formatErrorMessage = ( text ) => {
+  return text.replace( /\((\d+)/g, `( Line $1` )
+    .replace( /\:(\d+)/g, `: Col $1` )
 }
 
-const recoverRegex = (text, arrayOfRegex) => {
-  if (arrayOfRegex != null && arrayOfRegex.length > 0) {
+const recoverRegex = ( text, arrayOfRegex ) => {
+  if ( arrayOfRegex != null && arrayOfRegex.length > 0 ) {
     let positionArray = 0
-    text = text.replace(/\/.*\/\s?\w*/g, (str) => {
+    text = text.replace( /\/.*\/\s?\w*/g, ( str ) => {
       positionArray++
-      return arrayOfRegex[positionArray - 1]
-    })
+      return arrayOfRegex[ positionArray - 1 ]
+    } )
   }
   return text
 }
 
-const checkIfCodeHasVar = (text) => {
-  let indexVar = text.search(/(\svar\s)|(^var)/)
-  if (indexVar !== -1) {
-    let linesArray = text.substring(0, indexVar).match(/\n/g)
+const checkIfCodeHasVar = ( text ) => {
+  let indexVar = text.search( /(\svar\s)|(^var)/ )
+  if ( indexVar !== -1 ) {
+    let linesArray = text.substring( 0, indexVar + 1 ).match( /\n/g )
     let numberOfLines = linesArray == null ? 1 : linesArray.length + 1
-    vscode.window.showWarningMessage(`Are you dumb? Use "let" or "const" instead of "var". Line: ${numberOfLines}`)
+    vscode.window.showWarningMessage( `Are you dumb? Use "let" or "const" instead of "var". Line: ${ numberOfLines }` )
   }
 }
 
-function suissize(option) {
+function suissize ( option ) {
   const editor = vscode.window.activeTextEditor
-  if (!editor) return
+  if ( !editor ) return
 
-  vscode.commands.executeCommand('acceptSelectedSuggestion').then(() => {
+  vscode.commands.executeCommand( 'acceptSelectedSuggestion' ).then( () => {
     const document = editor.document
-    const start = new vscode.Position(0, 0)
-    const end = new vscode.Position(document.lineCount - 1,
-      document.lineAt(document.lineCount - 1).text.length)
-    let range = new vscode.Range(start, end)
-    let currentText = editor.document.getText(range)
+    const start = new vscode.Position( 0, 0 )
+    const end = new vscode.Position( document.lineCount - 1,
+      document.lineAt( document.lineCount - 1 ).text.length )
+    let range = new vscode.Range( start, end )
+    let currentText = editor.document.getText( range )
 
     let options = vscode.FormattingOptions
-    if (!options) {
+    if ( !options ) {
       options = { insertSpaces: true, tabSize: 2 }
     }
 
-    if (!currentText.isEmptyOrWhitespace) {
-      let insertionSuccess = editor.edit((editBuilder) => {
+    if ( !currentText.isEmptyOrWhitespace ) {
+      let insertionSuccess = editor.edit( ( editBuilder ) => {
         let formatted
         try {
-         currentText = protectArrowFunction(currentText)
-          formatted = standardFormat.transform(currentText)
-        } catch (err) {
-          vscode.window.showErrorMessage(formatErrorMessage(err.message))
+          currentText = protectArrowFunction( currentText )
+          formatted = standardFormat.transform( currentText )
+        } catch ( err ) {
+          vscode.window.showErrorMessage( formatErrorMessage( err.message ) )
           return
         }
-        let arrayOfRegex = formatted.match(/\/.*\/\s?\w*/g)
-        formatted = replaceParenthesisAndBrackets(formatted)
-        formatted = removeMultiLine(formatted)
-        formatted = unproctectArrowFunction(formatted)
-        formatted = fixSemiColon(formatted)
-        formatted = recoverRegex(formatted, arrayOfRegex)
+        let arrayOfRegex = formatted.match( /\/.*\/\s?\w*/g )
+        formatted = replaceParenthesisAndBrackets( formatted )
+        formatted = removeMultiLine( formatted )
+        formatted = unproctectArrowFunction( formatted )
+        formatted = fixSemiColon( formatted )
+        formatted = recoverRegex( formatted, arrayOfRegex )
 
-        editBuilder.replace(range, formatted)
-        checkIfCodeHasVar(formatted)
+        editBuilder.replace( range, formatted )
+        checkIfCodeHasVar( formatted )
+      } )
 
-      })
-
-      if (!insertionSuccess) return
+      if ( !insertionSuccess ) return
     }
-  })
+  } )
 }
 
-function activate(context) {
-  const formatIndentCode = vscode.commands.registerCommand('suissize.formatIndentCode', () => {
-    suissize('formatIndentCode')
-  })
-  context.subscriptions.push(formatIndentCode)
+function activate ( context ) {
+  const formatIndentCode = vscode.commands.registerCommand( 'suissize.formatIndentCode', () => {
+    suissize( 'formatIndentCode' )
+  } )
+  context.subscriptions.push( formatIndentCode )
 }
 
 exports.activate = activate
